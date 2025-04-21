66f08a662d35951bcf5195c5ddd777ab74e75d2e
Nguyen Ba Cong <congngba.21@gmail.com>
2025-04-21 08:29:44 +0700

    delete more notes

--- hash_model.py
+++ hash_model.py
@@ -1,12 +1,3 @@
-# ================================================================================================
-# Code for paper 'Deep Supervised Auto-encoder Hashing for Image Retrieval'.
-# Currently, you may not use this file for any purpose unless you got permission from the author.
-# Author: Sanli Tang
-# Email: tangsanli@sjtu.edu.cn
-# Organization: Shanghai Jiaotong Univ.
-# Modified Time: 2017/10/25
-# ================================================================================================
-
 import keras
 from keras import backend as K
 from tensorflow.keras.backend import mean, square, sum
